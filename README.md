b98aa66af506c0d45107c30aadf81dd54d771dfe
YamiOdymel <yamiodymel@gmail.com>
2021-12-02 02:44:45 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+npm install -g http-server
+http-server -c-1 -p 8081
+```
---
+++ js-pagination.js
@@ -0,0 +1,62 @@
+export class Pagination extends EventTarget {
+    constructor(options) {
+        super();
+        this.count_per_page = options?.count_per_page !== undefined ? options?.count_per_page : 0;
+        this.reset_on_update = options?.reset_on_update !== undefined ? options?.reset_on_update : false;
+        this.total = 0;
+        this.current_page = 1;
+        this.total_pages = 0;
+    }
+
+    // update
+    update(total) {
+        if (total === this.total) {
+            return this
+        }
+        this.total = total;
+        if (total === 0) {
+            this.total_pages = 0;
+        } else if (total < this.count_per_page) {
+            this.total_pages = 1;
+        } else {
+            this.total_pages = Math.ceil(total / this.count_per_page);
+        }
+        this.dispatchEvent(new Event('change'));
+        return this
+    }
+
+    // next
+    next() {
+        if (this.current_page + 1 > this.total_pages) {
+            return this
+        }
+        this.current_page++;
+        this.dispatchEvent(new Event('next'));
+        this.dispatchEvent(new Event('change'));
+        return this
+    }
+
+    // previous
+    previous() {
+        if (this.current_page - 1 === 0) {
+           return this
+        }
+        this.current_page--;
+        this.dispatchEvent(new Event('previous'));
+        this.dispatchEvent(new Event('change'));
+        return this
+    }
+
+    // goto
+    goto(page) {
+        if (page > this.total_pages || page < this.total_pages) {
+           return this
+        }
+        this.current_page = page;
+        this.dispatchEvent(new Event('goto'));
+        this.dispatchEvent(new Event('change'));
+        return this
+    }
+
+
+}
